9d146be06ed98586edbd46b341c1c34d72186652
koakpet <koakpet@yahoo.co.uk>
2024-09-03 14:53:16 +0100

    working on modal

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var double_close = document.getElementById("double_close");
+var glass_close = document.getElementById("glass_close");
+var planate_close = document.getElementById("planate_close");
+
+var double_door_btn = document.getElementById("double_door_btn");
+var glass_insert_btn = document.getElementById("glass_insert_btn");
+var planate_door_btn = document.getElementById("planate_door_btn");
+
+var double_door = document.getElementById("double_door");
+var glass_insert = document.getElementById("glass_insert");
+var planate_door = document.getElementById("planate_door");
+
+double_door_btn.onclick = function () {
+    double_door.style.display = "block";
+}
+
+double_close.onclick = function () {
+    double_door.style.display = "none";
+};
+
+glass_insert_btn.onclick = function () {
+    glass_insert.style.display = "block";
+};
+
+glass_close.onclick = function () {
+    glass_insert.style.display = "none";
+};
+
+planate_door_btn.onclick = function () {
+    planate_door.style.display = "block";
+};
+
+planate_close.onclick = function () {
+    planate_door.style.display = "none";
+}
