16e47b25ded36dc9fb00aa161529eb1be8da43e1
koakpet <koakpet@yahoo.co.uk>
2024-09-04 10:48:28 +0100

    mobile, working on get-in-touch section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-var double_close = document.getElementById("double_close");
-var glass_close = document.getElementById("glass_close");
-var planate_close = document.getElementById("planate_close");
+var double_close = document.getElementById("close-double-door");
+var glass_close = document.getElementById("close-glass-insert");
+var planate_close = document.getElementById("close-planate-door");
 
 var double_door_btn = document.getElementById("double_door_btn");
 var glass_insert_btn = document.getElementById("glass_insert_btn");
 var planate_door_btn = document.getElementById("planate_door_btn");
 
-var double_door = document.getElementById("double_door");
-var glass_insert = document.getElementById("glass_insert");
-var planate_door = document.getElementById("planate_door");
+var double_door = document.getElementById("modal-double-door");
+var glass_insert = document.getElementById("modal-glass-insert");
+var planate_door = document.getElementById("modal-planate-door");
 
 double_door_btn.onclick = function () {
     double_door.style.display = "block";
